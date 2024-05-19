41124d247d57e48da199d0e7324cce268e919dfa
jagdishharwani2001 <164191475+jagdishharwani2001@users.noreply.github.com>
2024-05-19 00:28:24 -0400

    Create index.js
    index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDom from "react-dom"
+
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import App from "./App.jsx"
+
+ReactDom.render(<>
+  
+   <App/>
+</>,document.getElementById("root"))
